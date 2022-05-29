286c36869d0cfdfbfebe4e1e32afaa4165fb7ea4
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-05-29 00:49:57 -0500

    completes degugging section

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -51,6 +53,9 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
+
+  binding.pry
+
   greeting = "WASSAP, #{name}!"
 end
