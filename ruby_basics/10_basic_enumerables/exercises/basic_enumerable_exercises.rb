049bfc253ad45cc57d90bebb9724e93a8844cf72
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-05-31 22:34:42 -0500

    passes all but last enumerable methods exercises

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,22 +1,28 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
+  inventory_list.each { |item, qty| puts "#{item}, quantity: #{qty}" }  
 end
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index do |item, index|  
+    puts "Guess ##{index + 1} is #{item}" 
+  end
 end
 
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
   # return an array of absolute values of each number
+  absolute_values = numbers.map(&:abs) 
 end
 
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
+  inventory_list.select { |key, val| [key, val] if val < 4 }.to_h 
 end
 
 def find_word_lengths(word_list)
